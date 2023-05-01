bbef40e9ca6a999aa3db49b1bf2f9c266a6bab60
Julien Bernard <artessbm@gmail.com>
2023-05-01 16:43:04 +0200

    Add project files.

---
+++ AvaloniaPad/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace AvaloniaPad.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<LineViewModel> Lines { get; set; }
+
+        public MainWindowViewModel() {
+            _lineViewModel = new LineViewModel();
+            Lines = new ObservableCollection<LineViewModel>() {_lineViewModel };
+        }
+
+        private LineViewModel _lineViewModel;
+    }
+
+    public partial class LineViewModel : ViewModelBase {
+
+        public LineViewModel()
+        {
+            Option = new ObservableCollection<string>() { "Foo", "Bar" };
+        }
+
+        [ObservableProperty]
+        private int _index;
+
+        public ObservableCollection<string> Option { get; set; }
+    }
+}
---
+++ AvaloniaPad/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaPad.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
