244d9a05c18ce4d9ac4d86c2ee5fb8330671ef34
Julien Bernard <artessbm@gmail.com>
2023-05-03 16:32:13 +0200

    TransientState fix attempts

--- VVMSwitching/ViewModels/MainWindowViewModel.cs
+++ VVMSwitching/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,63 @@
-﻿namespace VVMSwitching.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VVMSwitching.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        [ObservableProperty]
+        private int _choice = 0;
+
+        [ObservableProperty]
+        private object _subViewModel;
+
+        private ViewModelA vma = new ViewModelA();
+        private ViewModelB vmb = new ViewModelB();
+
+        public MainWindowViewModel()
+        {
+            _subViewModel = vma;
+        }
+
+        public void Change()
+        {
+            if (Choice == 0)
+            {
+                Choice = -1;
+                SubViewModel = vmb;
+                Choice = 1;
+            }
+            else
+            {
+                Choice = -1;
+                SubViewModel = vma;
+                Choice = 0;
+            }
+            /*if (Choice == 0)
+            {
+                _subViewModel = vmb;
+                Choice = 1;
+                OnPropertyChanged(nameof(SubViewModel));
+            }
+            else
+            {
+                _subViewModel = vma;
+                Choice = 0;
+                OnPropertyChanged(nameof(SubViewModel));
+            }*/
+        }
     }
+
+    public class ViewModelA : ViewModelBase
+    {
+        public string PropertyA => "Property A";
+    }
+
+    public class ViewModelB : ViewModelBase
+    {
+        public string PropertyB => "Property B";
+    }
+
+
 }
